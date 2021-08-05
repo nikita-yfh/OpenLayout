f9b7715dcc459c064acbc82bef657eb3f5e53417
nikita-yfh <nikita.yfh@gmail.com>
2021-08-05 20:25:18 +0000

    Rewriten ProjectInfoDialog

--- ProjectInfoDialog.cpp
+++ ProjectInfoDialog.cpp
@@ -1,91 +1,63 @@
 #include "ProjectInfoDialog.h"
 
-//(*InternalHeaders(ProjectInfoDialog)
 #include <wx/button.h>
 #include <wx/intl.h>
 #include <wx/string.h>
-//*)
-
-//(*IdInit(ProjectInfoDialog)
-const long ProjectInfoDialog::ID_STATICTEXT2 = wxNewId();
-const long ProjectInfoDialog::ID_STATICTEXT3 = wxNewId();
-const long ProjectInfoDialog::ID_STATICTEXT4 = wxNewId();
-const long ProjectInfoDialog::ID_TEXTCTRL2 = wxNewId();
-const long ProjectInfoDialog::ID_TEXTCTRL3 = wxNewId();
-const long ProjectInfoDialog::ID_TEXTCTRL4 = wxNewId();
-const long ProjectInfoDialog::ID_STATICTEXT1 = wxNewId();
-const long ProjectInfoDialog::ID_TEXTCTRL1 = wxNewId();
-//*)
-
-BEGIN_EVENT_TABLE(ProjectInfoDialog,wxDialog)
-    //(*EventTable(ProjectInfoDialog)
-    //*)
-END_EVENT_TABLE()
-
-ProjectInfoDialog::ProjectInfoDialog(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size) {
-    //(*Initialize(ProjectInfoDialog)
-    wxBoxSizer* BoxSizer1;
-    wxBoxSizer* BoxSizer2;
-    wxBoxSizer* BoxSizer3;
-    wxBoxSizer* BoxSizer4;
-    wxStaticBoxSizer* StaticBoxSizer1;
-    wxStdDialogButtonSizer* StdDialogButtonSizer1;
-
-    Create(parent, id, _("Project info"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER, _T("id"));
-    SetClientSize(wxDefaultSize);
-    Move(wxDefaultPosition);
-    BoxSizer1 = new wxBoxSizer(wxVERTICAL);
-    StaticBoxSizer1 = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Copyright"));
-    BoxSizer2 = new wxBoxSizer(wxHORIZONTAL);
-    BoxSizer3 = new wxBoxSizer(wxVERTICAL);
-    StaticText2 = new wxStaticText(this, ID_STATICTEXT2, _("Title:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
-    BoxSizer3->Add(StaticText2, 1, wxALL|wxEXPAND, 5);
-    StaticText3 = new wxStaticText(this, ID_STATICTEXT3, _("Author:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT3"));
-    BoxSizer3->Add(StaticText3, 1, wxALL|wxEXPAND, 5);
-    StaticText4 = new wxStaticText(this, ID_STATICTEXT4, _("Company:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT4"));
-    BoxSizer3->Add(StaticText4, 1, wxALL|wxEXPAND, 5);
-    BoxSizer2->Add(BoxSizer3, 0, wxALL|wxEXPAND, 5);
-    BoxSizer4 = new wxBoxSizer(wxVERTICAL);
-    TextTitle = new wxTextCtrl(this, ID_TEXTCTRL2, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
-    TextTitle->SetMaxLength(100);
-    BoxSizer4->Add(TextTitle, 1, wxALL|wxEXPAND, 5);
-    TextAuthor = new wxTextCtrl(this, ID_TEXTCTRL3, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL3"));
-    TextAuthor->SetMaxLength(100);
-    BoxSizer4->Add(TextAuthor, 1, wxALL|wxEXPAND, 5);
-    TextCompany = new wxTextCtrl(this, ID_TEXTCTRL4, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL4"));
-    TextCompany->SetMaxLength(100);
-    BoxSizer4->Add(TextCompany, 1, wxALL|wxEXPAND, 5);
-    BoxSizer2->Add(BoxSizer4, 1, wxALL|wxEXPAND, 5);
-    StaticBoxSizer1->Add(BoxSizer2, 1, wxALL|wxEXPAND, 5);
-    BoxSizer1->Add(StaticBoxSizer1, 0, wxALL|wxEXPAND, 5);
-    StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Comment:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
-    BoxSizer1->Add(StaticText1, 0, wxALL|wxEXPAND, 5);
-    TextComment = new wxTextCtrl(this, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    BoxSizer1->Add(TextComment, 1, wxALL|wxEXPAND, 5);
-    StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
-    StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_OK, wxEmptyString));
-    StdDialogButtonSizer1->Realize();
-    BoxSizer1->Add(StdDialogButtonSizer1, 1, wxALL|wxEXPAND, 5);
-    SetSizer(BoxSizer1);
-    BoxSizer1->Fit(this);
-    BoxSizer1->SetSizeHints(this);
-    //*)
+ProjectInfoDialog::ProjectInfoDialog(wxWindow* parent,const ProjectInfo &info):
+		wxDialog(parent,wxID_ANY,_("Project info"),wxDefaultPosition,wxDefaultSize,wxRESIZE_BORDER|0x20000000|0x800|0x1000) {
+	wxBoxSizer* all_box = new wxBoxSizer(wxVERTICAL);
+    {
+		wxStaticBoxSizer* copyright = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Copyright"));
+		{
+			wxBoxSizer* cr_box = new wxBoxSizer(wxHORIZONTAL);
+			{
+				wxBoxSizer* text_box = new wxBoxSizer(wxVERTICAL);
+				text_box->Add(new wxStaticText(this, wxID_ANY, _("Title:")), 1, wxALL|wxEXPAND, 5);
+				text_box->Add(new wxStaticText(this, wxID_ANY, _("Author:")), 1, wxALL|wxEXPAND, 5);
+				text_box->Add(new wxStaticText(this, wxID_ANY, _("Company:")), 1, wxALL|wxEXPAND, 5);
+				cr_box->Add(text_box, 0, wxALL|wxEXPAND, 5);
+			}
+			{
+				wxBoxSizer* input_box = new wxBoxSizer(wxVERTICAL);
+				{
+					TextTitle = new wxTextCtrl(this, wxID_ANY, info.title);
+					TextTitle->SetMaxLength(100);
+					input_box->Add(TextTitle, 1, wxALL|wxEXPAND, 5);
+				}
+				{
+					TextAuthor = new wxTextCtrl(this, wxID_ANY, info.author);
+					TextAuthor->SetMaxLength(100);
+					input_box->Add(TextAuthor, 1, wxALL|wxEXPAND, 5);
+				}
+				{
+					TextCompany = new wxTextCtrl(this, wxID_ANY, info.company);
+					TextCompany->SetMaxLength(100);
+					input_box->Add(TextCompany, 1, wxALL|wxEXPAND, 5);
+				}
+				cr_box->Add(input_box, 3, wxALL|wxEXPAND, 5);
+			}
+			copyright->Add(cr_box, 1, wxALL|wxEXPAND, 5);
+		}
+		all_box->Add(copyright, 0, wxALL|wxEXPAND, 5);
+    }
+    {
+		all_box->Add(new wxStaticText(this, wxID_ANY, _("Comment:")), 0, wxALL|wxEXPAND, 5);
+		TextComment = new wxTextCtrl(this, wxID_ANY, info.comment,wxDefaultPosition,wxDefaultSize,wxTE_MULTILINE);
+		all_box->Add(TextComment, 1, wxALL|wxEXPAND, 10);
+    }
+    {
+		wxStdDialogButtonSizer* buttons = new wxStdDialogButtonSizer();
+		buttons->AddButton(new wxButton(this, wxID_OK, wxEmptyString));
+		buttons->Realize();
+		all_box->Add(buttons, 0, wxALL|wxEXPAND, 5);
+    }
+    SetSizerAndFit(all_box);
 }
 
-void ProjectInfoDialog::Set(const ProjectInfo info) {
-    TextTitle->SetValue(info.title);
-    TextAuthor->SetValue(info.author);
-    TextCompany->SetValue(info.company);
-    TextComment->SetValue(info.comment);
-}
 void ProjectInfoDialog::Get(ProjectInfo &info) {
     strcpy(info.title,TextTitle->GetValue().c_str());
     strcpy(info.author,TextAuthor->GetValue().c_str());
     strcpy(info.company,TextCompany->GetValue().c_str());
     strcpy(info.comment,TextComment->GetValue().c_str());
-}
-ProjectInfoDialog::~ProjectInfoDialog() {
-    //(*Destroy(ProjectInfoDialog)
-    //*)
 }
 
--- ProjectInfoDialog.h
+++ ProjectInfoDialog.h
@@ -2,49 +2,20 @@
 #define PROJECTINFO_H
 #include "PCBFile.h"
 
-//(*Headers(ProjectInfoDialog)
 #include <wx/dialog.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
 #include <wx/textctrl.h>
-//*)
 
 class ProjectInfoDialog: public wxDialog {
 public:
-    void Set(const ProjectInfo info);
     void Get(ProjectInfo &info);
-    ProjectInfoDialog(wxWindow* parent,wxWindowID id=wxID_ANY,const wxPoint& pos=wxDefaultPosition,const wxSize& size=wxDefaultSize);
-    virtual ~ProjectInfoDialog();
+    ProjectInfoDialog(wxWindow* parent,const ProjectInfo &info);
 
-    //(*Declarations(ProjectInfoDialog)
-    wxStaticText* StaticText1;
-    wxStaticText* StaticText2;
-    wxStaticText* StaticText3;
-    wxStaticText* StaticText4;
     wxTextCtrl* TextAuthor;
     wxTextCtrl* TextComment;
     wxTextCtrl* TextCompany;
     wxTextCtrl* TextTitle;
-    //*)
-
-protected:
-
-    //(*Identifiers(ProjectInfoDialog)
-    static const long ID_STATICTEXT2;
-    static const long ID_STATICTEXT3;
-    static const long ID_STATICTEXT4;
-    static const long ID_TEXTCTRL2;
-    static const long ID_TEXTCTRL3;
-    static const long ID_TEXTCTRL4;
-    static const long ID_STATICTEXT1;
-    static const long ID_TEXTCTRL1;
-    //*)
-
-private:
-    //(*Handlers(ProjectInfoDialog)
-    //*)
-
-    DECLARE_EVENT_TABLE()
 };
 
 #endif
