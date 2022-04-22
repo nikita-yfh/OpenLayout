9fadadedb733eb8159ddf80ab23b5299eefe365f
nikita-yfh <nikita.yfh@gmail.com>
2022-04-22 12:43:13 +0300

    Add NewBoardDialog

--- src/PCB.h
+++ src/PCB.h
@@ -12,12 +12,12 @@ public:
 	void Save(File &file) const;
 	bool Load(File &file);
 
+	void AddBoard(Board *board);
 	uint32_t GetBoardCount() const;
 	void SetBoard(uint32_t n);
 	Board *GetSelectedBoard();
 private:
 	Board *GetLastBoard();
-	void AddBoard(Board *board);
 
 	const uint32_t magic=0xFFAA3306;
 	Board *boards;
