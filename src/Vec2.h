7f14cbfeab1218ace17e22526146789e11258f80
nikita-yfh <nikita.yfh@gmail.com>
2022-04-23 19:01:23 +0300

    Add AABB class

--- src/Vec2.h
+++ src/Vec2.h
@@ -7,65 +7,65 @@ struct Vec2 {
 	float y;
 
 	Vec2() {}
-	Vec2(float _x,float _y){
-		Set(_x,_y);
+	Vec2(float _x,float _y) {
+		Set(_x, _y);
 	}
 
-	void Set(float _x,float _y){
-		x=_x;
-		y=_y;
+	void Set(float _x,float _y) {
+		x = _x;
+		y = _y;
 	}
-	void SetZero(){
-		x=y=0.0f;
+	void SetZero() {
+		x = y =0.0f;
 	}
-	float Length() const{
-		return sqrt(x*x+y*y);
+	float Length() const {
+		return sqrt(x*x + y*y);
 	}
-	float LengthSq() const{
-		return x*x+y*y;
+	float LengthSq() const {
+		return x*x + y*y;
 	}
 
-	Vec2 operator-() const{
-		return Vec2(-x,-y);
+	Vec2 operator-() const {
+		return Vec2(-x, -y);
 	}
-	Vec2 operator~() const{
-		return Vec2(-y,x);
+	Vec2 operator~() const {
+		return Vec2(-y, x);
 	}
-	Vec2 operator+(const Vec2 &v) const{
-		return Vec2(x+v.x,y+v.y);
+	Vec2 operator+(const Vec2 &v) const {
+		return Vec2(x+v.x, y+v.y);
 	}
-	Vec2 operator-(const Vec2 &v) const{
-		return Vec2(x-v.x,y-v.y);
+	Vec2 operator-(const Vec2 &v) const {
+		return Vec2(x-v.x, y-v.y);
 	}
-	Vec2 operator*(float k) const{
-		return Vec2(x*k,y*k);
+	Vec2 operator*(float k) const {
+		return Vec2(x*k, y*k);
 	}
-	Vec2 operator/(float k) const{
-		return Vec2(x/k,y/k);
+	Vec2 operator/(float k) const {
+		return Vec2(x/k, y/k);
 	}
-	void operator+=(const Vec2 &v){
-		x+=v.x;
-		y+=v.y;
+	void operator+=(const Vec2 &v) {
+		x += v.x;
+		y += v.y;
 	}
-	void operator-=(const Vec2 &v){
-		x-=v.x;
-		y-=v.y;
+	void operator-=(const Vec2 &v) {
+		x -= v.x;
+		y -= v.y;
 	}
-	void operator*=(const float &v){
-		x*=v;
-		y*=v;
+	void operator*=(const float &v) {
+		x *= v;
+		y *= v;
 	}
-	void operator/=(const float &v){
-		x/=v;
-		y/=v;
+	void operator/=(const float &v) {
+		x /= v;
+		y /= v;
 	}
 	template<typename T>
-	void Save(File &file) const{
+	void Save(File &file) const {
 		file.WriteMm<T>(x);
 		file.WriteMm<T>(y);
 	}
 	template<typename T>
-	void Load(File &file){
+	void Load(File &file) {
 		x = file.ReadMm<T>();
 		y = file.ReadMm<T>();
 	}
