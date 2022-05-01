52a9e4e95f949f823a74dd7e554937b7ad56c7bb
nikita-yfh <nikita.yfh@gmail.com>
2022-05-01 20:40:02 +0300

    Add save/load of arcs and circles

---
+++ src/Arc.cpp
@@ -0,0 +1,57 @@
+#include "Arc.h"
+
+AABB Arc::GetAABB() const {
+	Vec2 radius((diameter + width) * 0.5f, (diameter + width) * 0.5f);
+	return AABB(position - radius, position + radius);
+}
+
+void Arc::SaveObject(File &file) const {
+	file.Write<uint8_t>(CIRCLE);
+	position.Save<float>(file);
+	file.WriteMm<float>((diameter + width) / 2.0f);
+	file.WriteMm<float>((diameter - width) / 2.0f);
+	file.WriteAngle<uint32_t>(endAngle);
+	file.WriteNull(1);
+	file.Write<uint8_t>(layer);
+	file.WriteNull(5);
+	file.Write<uint16_t>(componentID);
+	file.WriteNull(1);
+	file.WriteAngle<uint32_t>(beginAngle);
+	file.WriteNull(5);
+	file.Write<uint8_t>(fill);
+	file.WriteMm<uint32_t>(groundDistance);
+	file.WriteNull(7);
+	file.Write<uint8_t>(cutoff);
+	file.WriteNull(5);
+	file.Write<uint8_t>(soldermask);
+	file.WriteNull(22);
+	file.WriteString(marker);
+
+	groups.Save(file);
+}
+
+void Arc::LoadObject(File &file) {
+	position.Load<float>(file);
+	float outRadius = file.ReadMm<float>();
+	float inRadius = file.ReadMm<float>();
+	diameter = outRadius + inRadius;
+	width = outRadius - inRadius;
+	endAngle = file.ReadAngle<uint32_t>();
+	file.ReadNull(1);
+	layer = file.Read<uint8_t>();
+	file.ReadNull(5);
+	componentID = file.Read<uint16_t>();
+	file.ReadNull(1);
+	beginAngle = file.ReadAngle<uint32_t>();
+	file.ReadNull(5);
+	fill = file.Read<uint8_t>();
+	groundDistance = file.ReadMm<uint32_t>();
+	file.ReadNull(7);
+	cutoff = file.Read<uint8_t>();
+	file.ReadNull(5);
+	soldermask = file.Read<uint8_t>();
+	file.ReadNull(22);
+	file.ReadString(marker);
+
+	groups.Load(file);
+}
--- src/OpenLayoutMain.cpp
+++ src/OpenLayoutMain.cpp
@@ -65,10 +65,10 @@ OpenLayoutFrame::OpenLayoutFrame()
 	SetSizer(content);
 	SetAutoLayout(true);
 
-	File file("/home/nikita/Documents/test/poly.lay6", "rb");
+	File file("/home/nikita/Documents/test/arc.lay6", "rb");
 	pcb.Load(file);
 
-	File file2("/home/nikita/Documents/test/poly-d.lay6", "wb");
+	File file2("/home/nikita/Documents/test/arc-d.lay6", "wb");
 	pcb.Save(file2);
 }
 
