c5b4f53638376dccfc9e811bf82f8f155433919b
nikita-yfh <nikita.yfh@gmail.com>
2022-05-01 20:35:40 +0300

    Fix bug with save strings

--- src/File.cpp
+++ src/File.cpp
@@ -12,27 +12,28 @@ bool File::IsOk() const{
 	return file != nullptr;
 }
 void File::Write(const void *ptr, size_t size) {
-	fwrite(ptr, size, 1,file);
+	fwrite(ptr, size, 1, file);
 }
 void File::Read(void *ptr, size_t size) {
-	fread(ptr, size, 1,file);
+	fread(ptr, size, 1, file);
 }
 void File::WriteString(const char *str, uint8_t n) {
 	Write<uint8_t>(strlen(str));
-	fwrite(str, n,1, file);
+	fwrite(str, n, 1, file);
 }
 void File::ReadString(char *str, uint8_t n) {
-	Read<uint8_t>();
-	fread(str, n,1, file);
+	uint8_t len = Read<uint8_t>();
+	fread(str, n, 1, file);
+	str[len] = '\0';
 }
 void File::WriteString(const char *str) {
 	uint32_t len = strlen(str);
 	Write<uint32_t>(len);
-	fwrite(str, len, 1,file);
+	fwrite(str, len, 1, file);
 }
 void File::ReadString(char *str) {
 	uint32_t len = Read<uint32_t>();
-	fread(str, len, 1,file);
+	fread(str, len, 1, file);
 }
 void File::WriteNull(size_t num) {
 	uint8_t null = 0x00;
